1a6c0fefbb6c9f90afa5a2b2883d5cc906c77beb
ngnd.halo <92264722+NgD-HaLo@users.noreply.github.com>
2024-10-12 15:11:46 +0700

    [UPDATE]: Refactor App container

--- App/components/Core/AppContainer.tsx
+++ App/components/Core/AppContainer.tsx
@@ -1,48 +1,25 @@
 import {View, TouchableOpacity, StyleSheet} from 'react-native';
-import {colors, colorType} from '../../themes/color';
+import {colors} from '../../themes/color';
 import {ViewProps} from 'react-native-svg/lib/typescript/fabric/utils';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import {AppText} from './AppText';
 import Navigator from '../../navigation/NavigationService';
 import SvgComponent from '../../assets/svg';
-import SCREEN_NAME from '../../navigation/ScreenName';
-import ConfirmModal from './ConfirmModal';
-import {useState} from 'react';
 
 interface IAppContainer extends ViewProps {
   title: string;
   children: React.ReactNode;
   backButton: boolean;
-  backHome?: boolean;
+  haveRightButton?: boolean;
+  rightButton?: React.ReactNode;
 }
 
 export const AppContainer = ({
   title,
   children,
-  backHome,
   backButton,
+  haveRightButton = false,
+  rightButton,
 }: IAppContainer) => {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const handleConfirm = () => {
-    Navigator.navigateTo(SCREEN_NAME.ROOT.HOME_SCREEN);
-    setModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setModalVisible(false);
-  };
-  const BackHome = () => {
-    return (
-      <TouchableOpacity
-        style={styles.backBtn}
-        onPress={() => {
-          setModalVisible(true);
-        }}>
-        <SvgComponent name="CHECK" size={30} color={colors.white} />
-      </TouchableOpacity>
-    );
-  };
   const BackButton = () => {
     return (
       <TouchableOpacity style={styles.backBtn} onPress={Navigator.goBack}>
@@ -60,16 +37,14 @@ export const AppContainer = ({
               {title}
             </AppText>
           </View>
-          {backHome ? <BackHome /> : <></>}
+          {haveRightButton ? (
+            <View style={styles.backBtn}>{rightButton}</View>
+          ) : (
+            <></>
+          )}
         </View>
       </View>
       {children}
-      <ConfirmModal
-        message="Are you want to continue?"
-        visible={modalVisible}
-        onConfirm={handleConfirm}
-        onCancel={handleCancel}
-      />
     </View>
   );
 };
@@ -91,7 +66,6 @@ const styles = StyleSheet.create({
   nameScreen: {
     alignItems: 'center',
     justifyContent: 'center',
-    // width: '90%',
     flex: 1,
   },
 });
--- App/screens/AddNewCardScreen/view.tsx
+++ App/screens/AddNewCardScreen/view.tsx
@@ -5,7 +5,10 @@ import {AppText, SubmitButton} from '../../components';
 import {colors} from '../../themes/color';
 import {TCard} from '../../types/Card';
 import {AppContainer} from '../../components/Core/AppContainer';
-import {TTopic} from '../../types/Topic';
+import SCREEN_NAME from '../../navigation/ScreenName';
+import Navigator from '../../navigation/NavigationService';
+import ConfirmModal from '../../components/Core/ConfirmModal';
+import SvgComponent from '../../assets/svg';
 interface AddNewCardViewProps {
   topic;
   cardList: TCard[];
@@ -14,6 +17,27 @@ interface AddNewCardViewProps {
 
 const AddNewCardView = ({topic, cardList, addCard}: AddNewCardViewProps) => {
   const [cardContent, setCardContent] = useState<TCard['content']>('');
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleConfirm = () => {
+    Navigator.navigateTo(SCREEN_NAME.ROOT.HOME_SCREEN);
+    setModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setModalVisible(false);
+  };
+  const BackHome = () => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          setModalVisible(true);
+        }}>
+        <SvgComponent name="CHECK" size={30} color={colors.white} />
+      </TouchableOpacity>
+    );
+  };
   const handleSubmit = () => {
     let card: TCard = {
       idTopic: topic.id,
@@ -24,7 +48,11 @@ const AddNewCardView = ({topic, cardList, addCard}: AddNewCardViewProps) => {
   };
 
   return (
-    <AppContainer backButton={true} backHome={true} title="ADD FLASHCARD">
+    <AppContainer
+      backButton={true}
+      haveRightButton={true}
+      rightButton={<BackHome />}
+      title="ADD FLASHCARD">
       <View style={styles.container}>
         <View style={styles.addTopic}>
           <AppText fontWeight={900} color={colors.black} fontSize={27}>
@@ -58,6 +86,12 @@ const AddNewCardView = ({topic, cardList, addCard}: AddNewCardViewProps) => {
             <></>
           )}
         </View>
+        <ConfirmModal
+          message="Are you want to continue?"
+          visible={modalVisible}
+          onConfirm={handleConfirm}
+          onCancel={handleCancel}
+        />
       </View>
     </AppContainer>
   );
